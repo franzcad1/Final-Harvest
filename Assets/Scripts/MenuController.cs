8f321e4afdfc74d54604710511a723df78e95c5d
sydney <sydneyhuang@rogers.com>
2021-02-07 12:56:24 -0500

    Options menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Game play");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
