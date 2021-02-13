9b01137b8fc85d9688a5bc8e988a2558a0b84cfa
Kautuk <Kautuk.u@gmail.com>
2021-02-12 22:24:11 -0500

    Added pause menu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private bool isPaused;
+    [SerializeField] private GameObject pauseMenuCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
