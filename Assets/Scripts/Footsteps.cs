48cceab5c397ec8fbbc5b665c0315a987c16a93e
Franz <fcadien1@my.centennialcollege.ca>
2021-02-14 15:35:28 -0500

    Added Internal Documentation

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -2,6 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Final Harvest
+Franz Cadiente 301098663
+Sydney Huang 301068497
+Kautuk Udavant 301072587
+
+    Date last modified: 02/14/21
+
+ */
 public class Footsteps : MonoBehaviour
 {
     [SerializeField]
