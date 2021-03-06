77ef8e02a07cd46bcce00055a0d6c6e246482a40
Franz <fcadien1@my.centennialcollege.ca>
2021-03-06 17:40:13 -0500

    Fixed Goal

--- Assets/_Scripts/HorseBehaviour.cs
+++ Assets/_Scripts/HorseBehaviour.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HorseBehaviour : MonoBehaviour
 {
-
+    public GameObject player;
+    private bool isWinnable = false;
+    public Text winnableText;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,34 +18,36 @@ public class HorseBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isWinnable)
+        {
+            winnableText.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene("Game Over");
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+        else
+        {
+            winnableText.gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Player detected");
+            player = other.transform.gameObject;
+            isWinnable = true;
         }
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                SceneManager.LoadScene("Game Over");
-                Debug.Log("Player detected");
-            }
-        }
-    }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
+            isWinnable = false;
         }
     }
 }
