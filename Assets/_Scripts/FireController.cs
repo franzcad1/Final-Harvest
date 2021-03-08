dd67fd4af3b8f5ecde8e37aa1f21791044c902ae
sydney <sydneyhuang@rogers.com>
2021-03-08 09:02:47 -0500

    hazards functionallity

---
+++ Assets/_Scripts/FireController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    public PlayerBehaviour playerBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBehaviour = FindObjectOfType<PlayerBehaviour>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerBehaviour.TakeDamage(20);
+        }
+    }
+}
