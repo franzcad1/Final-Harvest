f194affa3bbfddabb2db24f048de91f0ce0a8be9
Kautuk <Kautuk.u@gmail.com>
2021-03-08 21:59:57 -0500

    fixed few bugs

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * Final Harvest
-Franz Cadiente 301098663
-Sydney Huang 301068497
-Kautuk Udavant 301072587
+    Final Harvest
+    Franz Cadiente 301098663
+    Sydney Huang 301068497
+    Kautuk Udavant 301072587
 
     Date last modified: 03/08/21
-
  */
 public class CameraController : MonoBehaviour
 {
